d4e4c17acfb203a55fd67aa7ae1a01e9b02e4de8
risk1996 <williamdariansutedjo@gmail.com>
2017-11-07 12:08:12 +0700

    Update DB

--- assets/sql/louvre.sql
+++ assets/sql/louvre.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
     email       VARCHAR(30)     PRIMARY KEY,
     CHECK email LIKE '?%@?%.??%',
     role        VARCHAR(9)      NOT NULL,
-    CHECK role IN ('admin', 'employee', 'buyer'),
+    CHECK role IN ('admin', 'manager', 'employee', 'buyer'),
     fname       VARCHAR(25)     NOT NULL,
     lname       VARCHAR(25),
     pass        CHAR(64)        NOT NULL,
@@ -17,32 +17,20 @@ CREATE TABLE book(
     isbn13      CHAR(13)        PRIMARY KEY,
     title       VARCHAR(50)     NOT NULL,
     price       DECIMAL(10,2)   NOT NULL,
-    CHECK price >= 0,
+    CHECK price >= 0.0,
     stock       SMALLINT        NOT NULL,
     CHECK stock >= 0,
-    cover       VARCHAR(50)     NOT NULL,
+    cover       VARCHAR(50),
     summary     TEXT,
     edition     VARCHAR(5),
     pages       SMALLINT        NOT NULL,
     CHECK pages > 0,
     pubdate     DATE            NOT NULL,
+    genre       VARCHAR(20)     NOT NULL,
+    author      VARCHAR(50)     NOT NULL,
     language    VARCHAR(12)     NOT NULL,
     format      VARCHAR(5)      NOT NULL,
     CHECK format IN ('PDF', 'EPUB', 'MOBI', 'DJVU', 'AZW3')
-);
-
-CREATE TABLE bookgenre(
-    isbn13      CHAR(13),
-    FOREIGN KEY(isbn13) REFERENCES book(isbn13),
-    genre       VARCHAR(20)     NOT NULL,
-    PRIMARY KEY(isbn13, genre)
-);
-
-CREATE TABLE bookauthor(
-    isbn13      CHAR(13),
-    FOREIGN KEY(isbn13) REFERENCES book(isbn13),
-    aname       VARCHAR(50)     NOT NULL,
-    PRIMARY KEY(isbn13, author)
 );
 
 CREATE TABLE bookreview(
@@ -56,17 +44,55 @@ CREATE TABLE bookreview(
     review      TEXT
 );
 
-CREATE TABLE transactions(
-    invoiceno   CHAR(12),
-    CHECK invoiceno REGEXP 'TR-[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9][0-9]',
+CREATE TABLE bookfeatured(
     isbn13      CHAR(13),
     FOREIGN KEY(isbn13) REFERENCES book(isbn13),
+    description TEXT,
+    until       DATE            NOT NULL,
+    CHECK until >= CURDATE()
+);
+
+CREATE TABLE bookpromotion(
+    isbn13      CHAR(13),
+    FOREIGN KEY(isbn13) REFERENCES book(isbn13),
+    discount    DECIMAL(5,2)    DEFAULT 0.0,
+    until       DATE            NOT NULL,
+    CHECK until >= CURDATE()
+);
+
+CREATE TABLE cart(
     email       VARCHAR(30),
     FOREIGN KEY(email) REFERENCES users(email),
-    PRIMARY KEY(invoiceno, isbn13, email),
+    isbn13      CHAR(13),
+    FOREIGN KEY(isbn13) REFERENCES book(isbn13),
+    PRIMARY KEY(email, isbn13),
     quantity    SMALLINT        NOT NULL,
     CHECK quantity > 0,
     discount    DECIMAL(5,2)    DEFAULT 0.0,
+    CHECK discount >= 0.0,
+    addded      DATETIME        NOT NULL,
+    CHECK adddate <= CURDATE()
+);
+
+CREATE TABLE transactions(
+    invoiceno   CHAR(12)        PRIMARY KEY,
+    CHECK invoiceno REGEXP 'TR-[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9][0-9]',
+    email       VARCHAR(30),
+    FOREIGN KEY(email) REFERENCES users(email),
+    paymethod   VARCHAR(15),
+    CHECK paymethod IN ('VISA', 'Master Card', 'PayPal', 'Bitcoin'),
     invdate     DATETIME        NOT NULL,
     CHECK invoice <= CURDATE()
+);
+
+CREATE TABLE transactionsdetail(
+    invoiceno   CHAR(12),
+    FOREIGN KEY(invoiceno) REFERENCES transactions(invoiceno),
+    isbn13      CHAR(13),
+    FOREIGN KEY(isbn13) REFERENCES book(isbn13),
+    PRIMARY KEY(invoiceno, isbn13),
+    quantity    SMALLINT        NOT NULL,
+    CHECK quantity > 0,
+    discount    DECIMAL(5,2)    DEFAULT 0.0,
+    CHECK discount >= 0.0
 );
