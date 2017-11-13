ebd4182cdd903e23956f9f036a46373ed6f9d694
risk1996 <williamdariansutedjo@gmail.com>
2017-11-13 13:44:16 +0700

    Login form

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -4,4 +4,18 @@
     <script src="assets/jquery/jquery-3.2.1.js"></script>
     <script src="assets/popper/popper.js"></script>
     <script src="assets/bootstrap-4.0.0-beta.2-dist/js/bootstrap.js"></script>
+    <script>
+        $(function () {
+            $('[data-toggle="popover"]').popover()
+        })
+        $('#popover').popover({ 
+            html : true,
+            title: function() {
+            return $("#popover-head").html();
+            },
+            content: function() {
+            return $("#popover-content").html();
+            }
+        });
+    </script>
 </body>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -26,22 +26,50 @@
                         <li class="nav-item <?php if($title=='Home')echo 'active'; ?>"><a href="<?php echo site_url(); ?>" class="nav-link">Home</a></li>
                         <li class="nav-item <?php if($title=='About')echo 'active'; ?>"><a href="<?php echo site_url('about'); ?>" class="nav-link">About</a></li>
                     </ul>
-                    <form action="" method="post" class="form-inline" style="margin-left: auto;">
+                    <span style="margin-left: auto;">
+                        <?php
+                            if(isset($_SESSION['logged'])){
+
+                            }
+                            else{
+                                echo '<button type="button" class="btn btn-outline-secondary" data-placement="bottom" data-toggle="popover" title="Login" data-content="';
+                                    echo '<form action=\'\' method=\'\'>';
+                                        echo '<div class=\'input-group\'>';
+                                            echo '<span class=\'input-group-addon\' id=\'basic-addon1\'><span class=\'fa fa-envelope\'></span></span>';
+                                            echo '<input type=\'text\' class=\'form-control\' placeholder=\'E-Mail\' aria-label=\'E-Mail\' aria-describedby=\'basic-addon1\'>';
+                                        echo '</div>';
+                                        echo '<div class=\'input-group\'>';
+                                            echo '<span class=\'input-group-addon\' id=\'basic-addon2\'><span class=\'fa fa-key\'></span></span>';
+                                            echo '<input type=\'password\' class=\'form-control\' placeholder=\'Password\' aria-label=\'Password\' aria-describedby=\'basic-addon2\'>';
+                                        echo '</div>';
+                                        echo '<button class=\'btn btn-secondary\' type=\'submit\' name=\'submit\'>';
+                                            $sp=20;
+                                            for($i=0; $i<$sp; $i++)echo '&nbsp;';
+                                            echo 'Login';
+                                            for($i=0; $i<$sp; $i++)echo '&nbsp;';
+                                        echo '</button>';
+                                    echo '</form>" id="popover">';
+                                echo '<span class="fa fa-sign-in"></span></button>';
+                            }
+                        ?>
+                    </span>
+                    &nbsp;&nbsp;
+                    <form action="" method="post" class="form-inline">
                         <div class="input-group">
                             <span class="input-group-addon" style="padding: 0 0 0 0;">
                                 <select class="form-control" name="searchby">
-                                    <option>Name</option>
+                                    <option>Title</option>
                                     <option>ISBN</option>
                                     <option>Author</option>
                                     <option>Genre</option>
                                 </select>
                             </span>
-                            <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="searchby">
+                            <input type="text" class="form-control" placeholder="Keyword" aria-label="Keyword" aria-describedby="searchby">
                         </div>
                         &nbsp;&nbsp;
-                        <button class="btn btn-secondary my-2 my-sm-0" type="submit" name="submit">Search</button>
+                        <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit" name="submit">Search</button>
                     </form>
                 </div>
             </div>
         </nav>
-    </header>    
+    </header>
