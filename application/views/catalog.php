3b24c4d473002a70a6c84954935ad3625f2b5a74
risk1996 <williamdariansutedjo@gmail.com>
2017-11-14 15:43:39 +0700

    Catalog Search Criteria

--- application/views/catalog.php
+++ application/views/catalog.php
@@ -3,47 +3,47 @@
     <div class="row">
         <div class="col-sm-3">
             <h3>Search Criteria</h3>
-            <div id="simple-search">
-                <form action="" method="post" class="form-inline">
-                    <div class="input-group">
-                        <span class="input-group-addon" style="padding: 0 0 0 0;">
-                            <select class="form-control" name="searchby">
-                                <option>Title</option>
-                                <option>ISBN</option>
-                                <option>Author</option>
-                                <option>Genre</option>
-                            </select>
-                        </span>
-                        <input type="text" class="form-control" placeholder="Keyword" aria-label="Keyword" aria-describedby="searchby">
-                    </div>
-                    <br><br><br>
-                    <button class="btn btn-secondary btn-block" type="submit" name="submit">Search</button>
-                </form>
-            </div>
-            <div id="advanced-search">
-                <form action="" method="">
-                    <div class="input-group">
-                        <span class="input-group-addon" id="titleaddon">Title</span>
-                        <input type="text" class="form-control" aria-describedby="titleaddon">
-                    </div>
-                    <br>
-                    <div class="input-group">
-                        <span class="input-group-addon" id="isbnaddon">ISBN</span>
-                        <input type="text" class="form-control" aria-describedby="isbnaddon">
-                    </div>
-                    <br>
-                    <div class="input-group">
-                        <span class="input-group-addon" id="authoraddon">Author</span>
-                        <input type="text" class="form-control" aria-describedby="titleaddon">
-                    </div>
-                    <br>
-                    <label for="pricerange">Price Range: <span id="slidervalue">$0 - $400</span></label>
-                    <br>
-                    <input class="slider" type="text" data-slider-ticks="[0, 100, 200, 300, 400]" data-slider-ticks-snap-bounds="30" data-slider-ticks-labels='["$0", "$100", "$200", "$300", "$400"]' data-slider-value="[0,400]" style="width:100%;"/>
-                    <br><br>
-                    <button class="btn btn-secondary btn-block" type="submit" name="submit">Search</button>
-                </form>
-            </div>
+            <form action="" method="">
+                <div class="input-group">
+                    <span class="input-group-addon" id="titleaddon" style="width: 70px;">Title</span>
+                    <input type="text" class="form-control" aria-describedby="titleaddon" placeholder="Keyword">
+                </div>
+                <br>
+                <div class="input-group">
+                    <span class="input-group-addon" id="isbnaddon" style="width: 70px;">ISBN</span>
+                    <input type="text" class="form-control" aria-describedby="isbnaddon" placeholder="Keyword">
+                </div>
+                <br>
+                <div class="input-group">
+                    <span class="input-group-addon" id="authoraddon" style="width: 70px;">Author</span>
+                    <input type="text" class="form-control" aria-describedby="titleaddon" placeholder="Keyword">
+                </div>
+                <br>
+                <label for="pricerange">Price Range: <span id="pricerange">$0 - $400</span></label>
+                <br>
+                <script src="<?php echo site_url(); ?>/assets/bootstrap-slider/bootstrap-slider.js"></script>
+                <input id="priceslider" data-slider-id="priceslider" type="text" data-slider-ticks="[0, 100, 200, 300, 400]" data-slider-ticks-snap-bounds="30" data-slider-ticks-labels='["$0", "$100", "$200", "$300", "$400"]' data-slider-value="[0,400]" style="width:100%;"/>
+                <script>
+                    var slider = new Slider('#priceslider');
+                    slider.on("slide", function(sliderValue) {
+                        var val = String(sliderValue).split(',');
+                        document.getElementById("pricerange").textContent = '$' + val[0] + ' - ' + '$' + val[1];
+                    });
+                </script>
+                <br>
+                <label for="pagesslider">Page Range: <span id="pagesrange">0 - 5000</span></label>
+                <br>
+                <input id="pagesslider" data-slider-id="pagesslider" type="text" data-slider-ticks="[0, 1000, 2000, 3000, 4000, 5000]" data-slider-ticks-snap-bounds="30" data-slider-ticks-labels='["0", "1000", "2000", "3000", "4000", "5000"]' data-slider-value="[0,5000]" style="width:100%;"/>
+                <script>
+                    var slider = new Slider('#pagesslider');
+                    slider.on("slide", function(sliderValue) {
+                        var val = String(sliderValue).split(',');
+                        document.getElementById("pagesrange").textContent = val[0] + ' - ' + val[1];
+                    });
+                </script>
+                <br><br>
+                <button class="btn btn-dark btn-block" type="submit" name="submit">Search</button>
+            </form>
         </div>
         <div class="col-sm-9">
             <h3>Search Result</h3>
