ff02e8a635ee914e2a695a064c11f67cebd007ce
Miqdude <miqdadabdurhman@gmail.com>
2017-12-07 16:30:08 +0700

    update user

--- application/views/user.php
+++ application/views/user.php
@@ -2,25 +2,34 @@
     <h2>Account Detail</h2>
     <div class="row">
         <div class="container" style="border-style:  solid;">
-           <div id="accordion" data-children=".item">
+            <div id="accordion" data-children=".item">
                 <div class="item">
-                    <a data-toogle="collapse" data-parent="#accordion" href="#userdetails" aria-expanded="true" aria-controls="userdetails">
-                        User Details
+                    <a data-toggle="collapse" data-parent="#accordion" href="#userdetails" aria-expanded="true" aria-controls="userdetails">
+                        <h3>USER DETAILS</h3>
                     </a>
-                    <div id="userdetails" class="collapse show" role="tabpanel">
+                    <div id="userdetails" class="collapse" role="tabpanel">
                         <div class="row">
-                            <div class="col-sm-5">
-                                <p>Name   : <?php echo $user['fname']." ".$user['lname']; ?></p>
+                            <div class="col-sm-8">
+                                <div class="row">
+                                    <div class="col-sm-2">
+                                        
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-sm-5">
-                                <p>Gender   : <?php echo $user['gender']; ?></p>
+                            <div class="col-sm-4">
+                                ahvdmhafdhfahdfakj
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-sm-5">
-                                <p>Email   : <?php echo $user['email']; ?></p>
-                            </div>
-                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#userbooks" aria-expanded="false" aria-controls="userbooks">
+                        <h3>USER BOOKS</h3>
+                    </a>
+                    <div id="userbooks" class="collapse" role="tabpanel">
+                        <p class="mb-3">
+                        Donec at ipsum dignissim, rutrum turpis scelerisque, tristique lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nec dui turpis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                        </p>
                     </div>
                 </div>
             </div>
