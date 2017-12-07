e1f6a1eee52ccffdf66715f6a6adae7ea20b1c68
Miqdude <miqdadabdurhman@gmail.com>
2017-12-07 15:54:43 +0700

    update user php

--- application/views/user.php
+++ application/views/user.php
@@ -2,32 +2,25 @@
     <h2>Account Detail</h2>
     <div class="row">
         <div class="container" style="border-style:  solid;">
-           <div class="container">
-                <div class="row">
-                        <div class="col-sm-10">
-                            <h4>User Details</h4>
+           <div id="accordion" data-children=".item">
+                <div class="item">
+                    <a data-toogle="collapse" data-parent="#accordion" href="#userdetails" aria-expanded="true" aria-controls="userdetails">
+                        User Details
+                    </a>
+                    <div id="userdetails" class="collapse show" role="tabpanel">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <p>Name   : <?php echo $user['fname']." ".$user['lname']; ?></p>
+                            </div>
+                            <div class="col-sm-5">
+                                <p>Gender   : <?php echo $user['gender']; ?></p>
+                            </div>
                         </div>
-                </div>
-                <div class="row">
-                        <div class="col-sm-5">
-                            <p>Name   : <?php echo $user['fname']." ".$user['lname']; ?></p>
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <p>Email   : <?php echo $user['email']; ?></p>
+                            </div>
                         </div>
-                        <div class="col-sm-5">
-                            <p>Gender   : <?php echo $user['gender']; ?></p>
-                        </div>
-                </div>
-                <div class="row">
-                        <div class="col-sm-5">
-                            <p>Email   : <?php echo $user['email']; ?></p>
-                        </div>
-                </div>
-            </div>
-        </div>
-        <div class="container" style="border-style: solid;">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-10">
-                        <h4>User Books</h4>
                     </div>
                 </div>
             </div>
